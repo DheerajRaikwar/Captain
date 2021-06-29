33a4cb32ab9fa33eabb79a2beb1d661d5d90aeda
DheerajRaikwar <dheerajraikwarindian@gmail.com>
2021-06-29 17:58:04 +0530

    Functionalilty to add or delete task from container

--- captain/src/TodoApp/index.js
+++ captain/src/TodoApp/index.js
@@ -1,66 +1,76 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import ListItem from '../Componants/comp/ListItem';
 
-const TodoApp =() =>{
-    const [inputList, setInputList] =useState("");
+const TodoApp = () => {
+    const [inputList, setInputList] = useState("");
     const [items, setItems] = useState([]);
 
-    const showEvent=(event)=>{
+    const showEvent = (event) => {
         setInputList(event.target.value);
     };
-    const onAddTask=()=>{
-        setItems((ptask)=>{
-            return [...ptask,inputList];
+    const onAddTask = () => {
+        setItems((ptask) => {
+            return [...ptask, inputList];
         });
-        setItems('');
-       
+
     };
 
-    return(
-    <>
-        <div className='todoDiv'>
-            <div className="Header">
-                Thanks for Visit!
-            </div>
-            <div className='topNav'>
-                <a href="#">Projects</a>
-                <a href="#"> About</a>
-                <a  href="#"> More</a>
-            </div>
-           
-            <div className="row">
-                <div className="col w-input">
-                    <p className="todoTitle" > Enter Your Task</p>
-                    
-                    <input type="text" className="myTask" value={inputList} placeholder="Enter task" onChange={showEvent}/><br/>
-                    <button className="add" onClick={onAddTask} >Add</button>
-                </div> 
-                 
-                <div className="col w-task">
-                <p className="todoTitle" > Your Task</p>
-                <ol className="taskList">
-                    
-                    {   
-                    items.map((taskval)=>
-                    {
-                        return <ListItem item={taskval}/>
-                    }
-                    )
-                    }
-                        
-                </ol>
-                </div>
-                <div className="col w-task">
-                <p className="todoTitle" > Done</p>
-                </div>
-            </div>
-            
-            
-            
-           
-        </div>
+    const doneItem =(id) =>{
+        console.log("item deleted");
+        setItems((ptask)=>{
+            return ptask.filter((arrE,index)=>{
+                return index!==id;
+            })
+        })
+    }
 
-    </>
+    return (
+        <>
+            <div className='todoDiv'>
+                <div className="Header">
+                    Thanks for Visit!
+            </div>
+                <div className='topNav'>
+                    <a href="#">Projects</a>
+                    <a href="#"> About</a>
+                    <a href="#"> More</a>
+                </div>
+
+                <div className="row">
+                    <div className="col w-input">
+                        <p className="todoTitle" > Enter Your Task</p>
+
+                        <input type="text" className="myTask" value={inputList} placeholder="Enter task" onChange={showEvent} /><br />
+                        <button className="add" onClick={onAddTask} >Add</button>
+                    </div>
+
+                    <div className="col w-task">
+                        <p className="todoTitle" > Your Task</p>
+                        <ul className="taskList">
+
+                            {
+                                items.map((taskval,index) => {
+                                    return <ListItem item={taskval}
+                                    key={index}
+                                    id={index}
+                                    onSelect={doneItem} />
+                                })
+                            }
+
+                        </ul>
+                    </div>
+                    <div className="col w-task">
+                        <p className="todoTitle" > Done</p>
+                        
+                    </div>
+                </div>
+
+
+
+
+            </div>
+
+        </>
     )
 
 }
